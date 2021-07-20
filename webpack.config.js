46a12c26c8b875a1b61cce837886c33ea9a9e45d
OttawaSTEM <ottawastem@gmail.com>
2021-07-20 14:00:09 -0400

    update title

--- webpack.config.js
+++ webpack.config.js
@@ -159,7 +159,7 @@ module.exports = [
             new HtmlWebpackPlugin({
                 chunks: ['lib.min', 'gui'],
                 template: 'src/playground/index.ejs',
-                title: 'Ottawa STEM Club',
+                title: 'Scratch - Ottawa STEM Club',
                 sentryConfig: process.env.SENTRY_CONFIG ? '"' + process.env.SENTRY_CONFIG + '"' : null
             }),
             new HtmlWebpackPlugin({
