2038215296123801ed9fb04acdba1d784caf4d29
OttawaSTEM <ottawastem@gmail.com>
2024-01-21 13:02:18 -0500

    update name

--- src/lib/modify/code_block.js
+++ src/lib/modify/code_block.js
@@ -1,3 +1,14 @@
 export default function (Blockly) {
+    Blockly.Arduino['arduino_pin_getServoOutput'] = function (block) {
+        var arg0 = block.getFieldValue('PIN') || 'A1';
+        var arg1 = Blockly.Arduino.valueToCode(block, 'OUT', Blockly.Arduino.ORDER_UNARY_POSTFIX) || 0;
+
+        Blockly.Arduino.includes_['include_servo'] = '#include <Servo.h>';
+        Blockly.Arduino.definitions_['definitions_servo' + arg0] = 'Servo servo_' + arg0 + ';';
+        Blockly.Arduino.setups_['setups_servo' + arg0] = 'servo_' + arg0 + '.attach' + '(' + arg0 + ');';
+
+        var code = 'servo_' + arg0 + '.write' + '(' + arg1 + ');\n';
+        return code;
+    };
 }
 
