eca0b2f5e69918c48a2d385d2e552164e11322fa
OttawaSTEM <ottawastem@gmail.com>
2024-01-26 16:18:44 -0500

    add release ps

---
+++ release.ps1
@@ -0,0 +1,19 @@
+cd ../scratch-arduino-blocks
+$scracth_arduino_blocks_version = (Get-Content -Raw -Path package.json | ConvertFrom-Json).version
+Write-Host $scracth_arduino_blocks_version
+cd ../scratch-arduino-gui
+npm i --package-lock-only scratch-arduino-blocks@$scracth_arduino_blocks_version
+
+cd ../scratch-arduino-l10n
+$scracth_arduino_l10n_version = (Get-Content -Raw -Path package.json | ConvertFrom-Json).version
+Write-Host $scracth_arduino_l10n_version
+cd ../scratch-arduino-gui
+npm i --package-lock-only scratch-arduino-l10n@$scracth_arduino_l10n_version
+
+cd ../scratch-arduino-vm
+$scracth_arduino_vm_version = (Get-Content -Raw -Path package.json | ConvertFrom-Json).version
+Write-Host $scracth_arduino_vm_version
+cd ../scratch-arduino-gui
+npm i --package-lock-only scratch-arduino-vm@$scracth_arduino_vm_version
+
+git commit -am "update blocks l10n vm"
